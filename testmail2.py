5b92ca7d1514db17c9ad232e6bc1c383039d2ae4
bobo213 <shuoshuorili@vbuluo.com>
2015-05-29 14:32:04 +0800

    send ip

--- testmail2.py
+++ testmail2.py
@@ -6,6 +6,7 @@ from email.Utils import formatdate
 from email.Header import Header
 import sys
 import time;
+import os
 
 sender = 'iknowing@163.com'
 receiver = 'liubo@vbuluo.com'
@@ -14,9 +15,9 @@ smtpserver = 'smtp.163.com'
 username = 'iknowing@163.com'
 password = '61659660'
 tet = time.strftime('%Y/%m/%d %H:%M:%S',time.localtime())
+internetip = os.popen("curl -s ifconfig.me").read()
 
-
-message = 'I send a message by Python.测试' + ' ' +tet
+message = 'I send a message by Python.测试' + ',时间' +tet+ ',公网IP地址为：' +internetip
 msg = MIMEText(message)
 msg['Subject'] = Header(subject+ tet, 'utf-8')
 print msg
